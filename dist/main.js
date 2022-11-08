8dcfebb2fab4b2e744a9e20b5d3b0c7ceb566a6c
Gustavo Morini <gustavommh@gmail.com>
2022-11-08 14:07:23 -0300

    types: union, boolean, array, tuple, object, enum

--- dist/main.js
+++ dist/main.js
@@ -7,5 +7,39 @@ class Product {
 }
 const ps5 = new Product("Playstation 5", 4000);
 const xbox = new Product("Xbox series X", 4000);
-console.log(ps5, xbox);
+console.log("-- class", ps5, xbox);
+let keyPix = "teste@teste.com";
+console.log("-- keyPix:", keyPix);
+let admin = true;
+console.log("-- boolean", admin);
+const category = ["comics", "horror", "comedy"];
+const moviesYears = [];
+for (let year = 1980; year <= new Date().getFullYear(); year++) {
+    moviesYears.push(year);
+}
+console.log("-- array ", moviesYears);
+let superset = ["typescript", 2011, "anders"];
+let daysWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+];
+console.log("--tuple", superset, daysWeek);
+const configServer = {
+    server: "Firestore",
+    ip: "127:0.0.0.1",
+    password: "123456",
+};
+console.log("--- object", configServer);
+var Permission;
+(function (Permission) {
+    Permission[Permission["ADMIN"] = 0] = "ADMIN";
+    Permission[Permission["USER"] = 1] = "USER";
+    Permission[Permission["READONLY"] = 2] = "READONLY";
+})(Permission || (Permission = {}));
+console.log("--enum", Permission);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+/*
+ * class
+ */
 class Product {
   name: string;
   value: number;
@@ -9,9 +12,66 @@ class Product {
 }
 
 const ps5 = new Product("Playstation 5", 4000);
-
 const xbox = new Product("Xbox series X", 4000);
+console.log("-- class", ps5, xbox);
 
-console.log(ps5, xbox);
+/*
+ * union types
+ */
+let keyPix: string | number = "teste@teste.com";
+console.log("-- keyPix:", keyPix);
 
-/** baana 123 */
+/*
+ * boolean
+ */
+let admin: boolean = true;
+console.log("-- boolean", admin);
+
+/*
+ * array
+ */
+const category: (string | number)[] = ["comics", "horror", "comedy"];
+const moviesYears: Array<number | string> = [];
+for (let year = 1980; year <= new Date().getFullYear(); year++) {
+  moviesYears.push(year);
+}
+console.log("-- array ", moviesYears);
+
+/*
+ * tuple
+ */
+let superset: [string, number, string] = ["typescript", 2011, "anders"];
+
+let daysWeek: [string, string, string, string, string, string, string] = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+console.log("--tuple", superset, daysWeek);
+
+/*
+ * objects
+ */
+const configServer: object = {
+  server: "Firestore",
+  ip: "127:0.0.0.1",
+  password: "123456",
+};
+
+console.log("--- object", configServer);
+
+/**
+ * enum
+ */
+enum Permission {
+  ADMIN,
+  USER,
+  READONLY,
+}
+
+console.log("--enum", Permission);
