6293deb13b1a2b90871101eff573143f12ab8e1e
Gustavo Morini <gustavommh@gmail.com>
2022-11-09 11:15:47 -0300

    interface array, recursive

--- dist/Interface.js
+++ dist/Interface.js
@@ -26,4 +26,40 @@ const curse1 = {
     workload: 10,
     classification: 5,
 };
+const sum1 = (num1, num2) => {
+    return num1 + num2;
+};
+const sum2 = (num1, num2) => {
+    return num1 + num2;
+};
+const multipliesNumbers = (a, b) => a * b;
+const dividedNumbers = (a, b) => a / b;
+const calc = {
+    sum: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiplies: multipliesNumbers,
+    divided: dividedNumbers,
+};
+const frontEnd = {
+    id: 1,
+    name: "Front-End",
+};
+const backEnd = {
+    id: 2,
+    name: "Back-End",
+};
+const css = {
+    id: 3,
+    name: "CSS",
+    belongs: frontEnd,
+};
+const php = {
+    id: 4,
+    name: "PHP",
+    belongs: backEnd,
+};
+const menu = {
+    categories: [frontEnd, backEnd, css, php],
+};
+const myTodos = ["javascript", "typescript", "php", 2000, true, backEnd];
 //# sourceMappingURL=Interface.js.map
--- src/Interface.ts
+++ src/Interface.ts
@@ -67,3 +67,89 @@ const curse1: ICurse = {
   workload: 10,
   classification: 5,
 };
+
+/**
+ * interface function
+ * define como uma função
+ * deve se comportar
+ */
+
+interface ISumNumbers {
+  (num1: number, num2: number): number;
+}
+// ex. 1
+const sum1: ISumNumbers = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+// ex. 2
+const sum2: ISumNumbers = (num1, num2) => {
+  return num1 + num2;
+};
+
+/**
+ * interface métodos
+ * define como os métodos devem se corportar
+ */
+
+interface ICalc {
+  sum(a: number, b: number): number;
+  subtract(a: number, b: number): number;
+  multiplies(a: number, b: number): number;
+  divided(a: number, b: number): number;
+}
+
+const multipliesNumbers = (a: number, b: number): number => a * b;
+const dividedNumbers = (a: number, b: number): number => a / b;
+
+const calc: ICalc = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiplies: multipliesNumbers,
+  divided: dividedNumbers,
+};
+
+/**
+ * interface recursiva
+ */
+interface ICategory {
+  id: number;
+  name: string;
+  belongs?: ICategory;
+}
+
+interface IMenu {
+  categories: ICategory[];
+}
+
+const frontEnd: ICategory = {
+  id: 1,
+  name: "Front-End",
+};
+
+const backEnd: ICategory = {
+  id: 2,
+  name: "Back-End",
+};
+
+const css: ICategory = {
+  id: 3,
+  name: "CSS",
+  belongs: frontEnd,
+};
+
+const php: ICategory = {
+  id: 4,
+  name: "PHP",
+  belongs: backEnd,
+};
+
+const menu: IMenu = {
+  categories: [frontEnd, backEnd, css, php],
+};
+
+/** interface array */
+interface ITodo {
+  [index: number]: string | number | boolean | ICategory;
+}
+
+const myTodos: ITodo = ["javascript", "typescript", "php", 2000, true, backEnd];
