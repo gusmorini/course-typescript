6077583e9a845562ae99465d944ee10cceed618f
Gustavo Morini <gustavommh@gmail.com>
2022-11-08 21:57:40 -0300

    void, never

--- dist/main.js
+++ dist/main.js
@@ -7,17 +7,13 @@ class Product {
 }
 const ps5 = new Product("Playstation 5", 4000);
 const xbox = new Product("Xbox series X", 4000);
-console.log("-- class", ps5, xbox);
 let keyPix = "teste@teste.com";
-console.log("-- keyPix:", keyPix);
 let admin = true;
-console.log("-- boolean", admin);
 const category = ["comics", "horror", "comedy"];
 const moviesYears = [];
 for (let year = 1980; year <= new Date().getFullYear(); year++) {
     moviesYears.push(year);
 }
-console.log("-- array ", moviesYears);
 let superset = ["typescript", 2011, "anders"];
 let daysWeek = [
     "Monday",
@@ -28,18 +24,39 @@ let daysWeek = [
     "Saturday",
     "Sunday",
 ];
-console.log("--tuple", superset, daysWeek);
 const configServer = {
     server: "Firestore",
     ip: "127:0.0.0.1",
     password: "123456",
 };
-console.log("--- object", configServer);
 var Permission;
 (function (Permission) {
     Permission[Permission["ADMIN"] = 0] = "ADMIN";
     Permission[Permission["USER"] = 1] = "USER";
     Permission[Permission["READONLY"] = 2] = "READONLY";
 })(Permission || (Permission = {}));
-console.log("--enum", Permission);
+let db = "mysql, 127.0.0.1, password";
+db = null;
+let myVariable = undefined;
+myVariable = "text in variable accepter";
+let value;
+let result;
+result = {
+    success: true,
+};
+result = "yes very nice bro";
+result = 200;
+const myConsole = console.log("Hello my friends");
+function consoleDate() {
+    console.log(Date.now());
+}
+function showError(message) {
+    throw new Error(message);
+}
+let n = 0;
+function loopInfinite() {
+    while (true) {
+        console.log(n++);
+    }
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -68,28 +68,57 @@ enum Permission {
 
 /**
  * null | undefined
+ * null valor inválido
+ * undefined valor não definido
  */
-
-const element: HTMLHeadingElement | null = document.querySelector("div");
+// const element: HTMLHeadingElement | null = document.querySelector("div");
 let db: string | null = "mysql, 127.0.0.1, password";
 db = null;
-
 let myVariable: undefined | string = undefined;
 myVariable = "text in variable accepter";
 
 /**
  * any
+ * usado para definir uma variavel para todos os tipos
+ * deve ser usado com cautela em casos específicos do
+ * código
  */
-
 let value: any;
 
 /**
  * unknown
+ * usado quando n se sabe
+ * o tipo de dados que vai conter a variável
+ * ex. retorno de uma api
  */
-
 let result: unknown;
 result = {
   success: true,
 };
 result = "yes very nice bro";
 result = 200;
+
+/**
+ * void
+ * cirado para especificar
+ * retorno de funções | métodos
+ * que não retorna valor
+ */
+const myConsole: void = console.log("Hello my friends");
+function consoleDate(): void {
+  console.log(Date.now());
+}
+
+/**
+ * never
+ * tipo de dados que nunca vai ter um retorno
+ */
+function showError(message: string): never {
+  throw new Error(message);
+}
+let n: number = 0;
+function loopInfinite(): never {
+  while (true) {
+    console.log(n++);
+  }
+}
